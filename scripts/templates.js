1c40f05deb8af3bd101dfab62e5de38ce65637ea
Jan Neumann <janerikneumann@gmx.de>
2026-01-21 16:46:52 +0100

    design fitting

--- scripts/templates.js
+++ scripts/templates.js
@@ -1,6 +1,5 @@
 function getDishHTML(index){
     return `        <div id="food_card" class="food_card">
-            <img src="" alt="food" id="first_card_section">
             <section id="second_card_section">
                <h3 id="dish_name">
                    ${myDishes[index].name}
@@ -14,7 +13,7 @@ function getDishHTML(index){
                <p id="price">
                     ${myDishes[index].price}
                </p>
-               <button onclick="addToBasket(${index})">add to basket</button>
+                <button onclick="addToBasket(${index})" class="add-btn"></button>
             </section>
 
         </div>`;
