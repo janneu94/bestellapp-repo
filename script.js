d13275d23d693fce243a7566bfe88130792050fb
Jan Neumann <janerikneumann@gmx.de>
2026-02-18 18:25:21 +0100

    added order confirmed dialog

--- script.js
+++ script.js
@@ -4,89 +4,48 @@ let basket = [];
 
 function init() {
   render();
-  renderBasket();  // ← DAS ist wichtig
+  renderBasket();
 }
 
 function render() {
-    const menu = document.getElementById("menu");
-    menu.innerHTML = "";
+  const menu = document.getElementById("menu");
+  menu.innerHTML = "";
 
-    for (let i = 0; i < myDishes.length; i++) {
-        menu.innerHTML += getDishHTML(i);
-    }
+  for (let i = 0; i < myDishes.length; i++) {
+    menu.innerHTML += getDishHTML(i);
+  }
 }
-
-// function addToBasket(index) {
-//     let dish = myDishes[index];
-
-//     let existingDish = basket.find(item => item.name === dish.name);
-
-//     if (existingDish) {
-//         existingDish.amount++;
-//     } else {
-//         basket.push({
-//             name: dish.name,
-//             price: dish.price,
-//             amount: 1
-//         });
-//     }
-
-//     renderBasket();
-// }
-
-// function renderBasket() {
-//     let basketContainer = document.getElementById("basket");
-//     basketContainer.innerHTML = "";
-
-//     for (let i = 0; i < basket.length; i++) {
-//         basketContainer.innerHTML += getBasketHTML(i);
-//     }
-
-//     renderTotal();
-// }
 
 function renderTotal() {
-    let total = 0;
+  let total = 0;
 
-    for (const item of basket) {
-        const dish = getDishById(item.dishId);
-        if (!dish) continue;
-        total += dish.price * item.amount;
-    }
+  for (const item of basket) {
+    const dish = getDishById(item.dishId);
+    if (!dish) continue;
+    total += dish.price * item.amount;
+  }
 
-    document.getElementById("total").innerText =
-        "Total: " + total.toFixed(2) + " €";
-}
-
-
-
-// function openDialog() {
-//     const dialogRef = document.getElementById("basket_dialog");
-//     dialogRef.showModal();
-
-//     document.getElementById('basket_mobile').innerHTML =
-//         document.getElementById('basket').innerHTML;
-
-//     document.getElementById('total_mobile').innerHTML =
-//         document.getElementById('total').innerHTML;
-
-// }
-
-function closeDialog() {
-    const dialogRef = document.getElementById("basket_dialog");
-    dialogRef.close();
-
+  document.getElementById("total").innerText =
+    "Total: " + total.toFixed(2) + " €";
 }
 
 function showPlus(dishId) {
-    document.getElementById("plus_btn" + dishId).classList.add("visible");
-    document.getElementById("add_btn" + dishId).classList.add("add_btn_clicked");
+  let plusBtn = document.getElementById("plus_btn" + dishId);
+  let addBtn = document.getElementById("add_btn" + dishId);
 
-    const i = getBasketIndexById(dishId);
-    const amount = i === -1 ? 0 : basket[i].amount;
+  plusBtn.classList.add("visible");
+  addBtn.classList.add("add_btn_clicked");
 
-    document.getElementById("add_btn" + dishId).innerHTML = "added " + amount;
+  let index = getBasketIndexById(dishId);
+  let amount = 0;
+
+  if (index !== -1) {
+    amount = basket[index].amount;
+  }
+
+  addBtn.innerHTML = "added " + amount;
 }
+
 
 function showMinus(dishId) {
   const i = getBasketIndexById(dishId);
@@ -106,32 +65,32 @@ function showMinus(dishId) {
 }
 
 function getDishById(id) {
-    return myDishes.find(a => a.id === id);
+  return myDishes.find(a => a.id === id);
 }
 
 function getBasketIndexById(dishId) {
-    return basket.findIndex(b => b.dishId === dishId);
+  return basket.findIndex(b => b.dishId === dishId);
 }
 
 function addToBasket(id) {
-    let basketId = getBasketIndexById(id);
+  let basketId = getBasketIndexById(id);
 
-    if (basketId === -1) basket.push({ dishId: id, amount: 1 });
-    else basket[basketId].amount++;
+  if (basketId === -1) basket.push({ dishId: id, amount: 1 });
+  else basket[basketId].amount++;
 
-    renderBasket();
-    showPlus(id);
+  renderBasket();
+  showPlus(id);
 }
 
 function reduceBasket(id) {
-    let basketId = getBasketIndexById(id);
-    if (basketId === -1) return;
+  let basketId = getBasketIndexById(id);
+  if (basketId === -1) return;
 
-    basket[basketId].amount--;
-    if (basket[basketId].amount === 0) basket.splice(basketId, 1);
+  basket[basketId].amount--;
+  if (basket[basketId].amount === 0) basket.splice(basketId, 1);
 
-    renderBasket();
-    showMinus(id);
+  renderBasket();
+  showMinus(id);
 }
 
 function renderBasket() {
@@ -157,22 +116,53 @@ function renderBasket() {
 }
 
 function proofAmount(dishId) {
-    let index = getBasketIndexById(dishId);
-    if (index === -1) return;
+  let index = getBasketIndexById(dishId);
+  if (index === -1) return;
 
-    let minusBtn = document.getElementById("minus_btn_" + dishId);
-    if (!minusBtn) return;
+  let minusBtn = document.getElementById("minus_btn_" + dishId);
+  if (!minusBtn) return;
 
-    if (basket[index].amount === 1) {
-        minusBtn.classList.add("trash_icon");
-        minusBtn.innerHTML = "";
-    } else {
-        minusBtn.classList.remove("trash_icon");
-         minusBtn.innerHTML = "-";
-    }
+  if (basket[index].amount === 1) {
+    minusBtn.classList.add("trash_icon");
+    minusBtn.innerHTML = "";
+  } else {
+    minusBtn.classList.remove("trash_icon");
+    minusBtn.innerHTML = "-";
+  }
 }
 
 function toggleBasket() {
   document.querySelector(".basket_overlay").classList.toggle("open");
   document.querySelector(".m_btn_basket").classList.toggle("active");
+}
+
+function buyNow() {
+  basket = [];
+  renderBasket();
+  resetAddButtons();
+  let basketOverlay = document.querySelector(".basket_overlay");
+  let mobileBtn = document.querySelector(".m_btn_basket");
+
+  basketOverlay.classList.remove("open");
+  mobileBtn.classList.remove("active");
+  let dialog = document.getElementById("order_confirmed");
+  dialog.showModal();
+}
+
+function closeDialog(event) {
+  event.preventDefault();
+  event.stopPropagation();
+  let dialog = document.getElementById("order_confirmed");
+  dialog.close();
+}
+
+function resetAddButtons() {
+  document.querySelectorAll(".add_btn_clicked").forEach(btn => {
+    btn.classList.remove("add_btn_clicked");
+    btn.innerHTML = "";
+  });
+
+  document.querySelectorAll(".plus_btn.visible").forEach(btn => {
+    btn.classList.remove("visible");
+  });
 }
